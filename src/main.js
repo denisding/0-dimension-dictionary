25003a2da79f94449472443696ba82ad19a93b1c
Denis Ding <yyguangzheng@163.com>
2021-03-28 17:35:38 +0800

    d

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
-import 'dayjs/locale/zh-cn'
-import locale from 'element-plus/lib/locale/lang/zh-cn'
 import App from './App.vue';
-import installElementPlus from './plugins/element'
 
-const app = createApp(App)
-app.use(ElementPlus, { locale })
-app.mount('#app')
+createApp(App).mount('#app')
